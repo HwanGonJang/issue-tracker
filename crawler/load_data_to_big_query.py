7598c1b9804e1e9b841289955d5589816e9a9fab
HwanGonJang <myggona@gmail.com>
2023-07-31 21:50:21 +0900

    fix: udf for timestamp

--- crawler/load_data_to_big_query.py
+++ crawler/load_data_to_big_query.py
@@ -2,6 +2,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import to_timestamp, udf
 
 import os
+from datetime import datetime, timedelta
 
 # 구글 클라우드 프로젝트 ID 설정
 os.environ["GOOGLE_CLOUD_PROJECT"] = "issue-tracker-394212"
@@ -15,6 +16,7 @@ spark = SparkSession.builder\
 # UDF 예시
 @udf('string')
 def datetime_to_string(dt):
+  dt -= timedelta(hours=9)
   return dt.strftime('%Y-%m-%d %H:%M:%S')
 
 spark.udf.register("datetime_to_string", datetime_to_string)
@@ -31,8 +33,8 @@ SELECT
     id,
     title,
     content,
-    article_written_at as article_written_at,
-    scraped_at as scraped_at,
+    to_timestamp(datetime_to_string(article_written_at)) as article_written_at,
+    to_timestamp(datetime_to_string(article_written_at)) as scraped_at,
     category,
     hits,
     url
